9d364ecbaba67c89b8898ecc6dd2053434d4e229
TonyNogueron <antonio.nogueron03@gmail.com>
2023-02-19 16:06:22 -0600

    adds main function and imports routes

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-//API
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const login = require("./routes/login");
+const user = require("./routes/user");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const CONFIG = require("./config/jwt");
+
+app.use(express.json());
+
+app.use("/", login);
+app.use("/", user);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
